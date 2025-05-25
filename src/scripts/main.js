403ffe9e7ccd968bb7a8356e80d77d2aa651d1fc
Thiago Sam <thiagosamthi@gmail.com>
2025-05-25 18:15:13 -0400

    Configuração da galeria, do rodapé e dos efeitos do menu.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
-function somar(a, b) {
-    return a + b;
+document.addEventListener('DOMContentLoaded', () => {
+    const heroSection = document.querySelector('.hero');
+    const header = document.querySelector('header')
+    const heroHeight = heroSection.clientHeight;
+
+    window.addEventListener('scroll', () => {
+    const heroPosition = window.scrollY;
+
+    if (heroPosition > heroHeight) {
+        header.classList.add('header--hidden');
+    } else {
+        header.classList.remove('header--hidden');
+    }
+})
+})
+
+function debounce(func, wait = 20) {
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
 }
