c47e2d77cf4d1fd3fe4d6659ccc62748b7da7037
Thiago Sam <thiagosamthi@gmail.com>
2025-05-26 11:30:32 -0400

    Configuração das âncoras dos menus, ajustes de media queries para tablet e configuração do javascript para efeitos do header e thumb de vídeo.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
     const heroSection = document.querySelector('.hero');
-    const header = document.querySelector('header')
+    const header = document.querySelector('header');
     const heroHeight = heroSection.clientHeight;
+
+    playVideo();
 
     window.addEventListener('scroll', () => {
     const heroPosition = window.scrollY;
 
     if (heroPosition > heroHeight) {
-        header.classList.add('header--hidden');
+        header.classList.add('header--hidden')
     } else {
-        header.classList.remove('header--hidden');
+        header.classList.remove('header--hidden')
     }
 })
+
 })
 
 function debounce(func, wait = 20) {
     let timeout;
-    return (...args) => {
+    return function(...args) {
+        const later = () => {
+            timeout = null;
+            func.apply(this, args);
+        };
         clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(this, args), wait);
+        timeout = setTimeout(later, wait);
     };
+}
+
+function playVideo() {
+    const videoThumb = document.querySelector('.hero__trailer__thumb');
+        videoThumb.addEventListener('click', () => {
+            videoThumb.classList.add('hero__trailer__thumb--none');
+            document.getElementById('video').src += '&autoplay=1';
+    })
 }
